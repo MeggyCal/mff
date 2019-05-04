1c7cd317f1468e314d924d94d939cf36f4746b27
David Nápravník <david.napravnik@bimproject.cz>
2019-05-04 21:33:13 +0200

    not working anymore

--- shaders/fragment/forward.glsl
+++ shaders/fragment/forward.glsl
@@ -1,29 +1,18 @@
 #version 300 es
 precision mediump float;
 
-struct light
-{
-	vec3 direction;
-	vec3 color;
-};
+in vec2 vertCoordinates;
 
-in vec4 fragTexCoord;
-in vec4 fragNormal;
-in vec4 fragPosition;
-
-uniform sampler2D sampler;
+uniform sampler2D uPositionBuffer;
+uniform sampler2D uNormalBuffer;
+uniform sampler2D uUVBuffer;
 
 out vec4 FragColor;
 
-vec3 ambientLightIntensity 	= vec3(0.2, 0.2, 0.2);
-light sun 		= light(normalize(vec3( 0.0, 0.0,-1.0)), vec3(0.8, 0.8, 0.8));
-
-
 void main()
 {
-	vec4 texture = texture(sampler, fragTexCoord.xy);
-	vec3 lightIntensity =
-		ambientLightIntensity +
-		sun.color 	* max(dot(fragNormal.xyz, sun.direction	), 0.0);
-	FragColor = vec4(texture.rgb * lightIntensity, texture.a);
+	vec4 texPos = texture(uPositionBuffer, vertCoordinates);
+	vec4 texNormal = texture(uNormalBuffer, vertCoordinates);
+	vec4 texUV = texture(uUVBuffer, vertCoordinates);
+	FragColor = vec4(texPos.rgb+texNormal.rgb+texUV.rgb, 1.0);
 }
--- shaders/vertex/forward.glsl
+++ shaders/vertex/forward.glsl
@@ -1,23 +1,12 @@
 #version 300 es
 precision mediump float;
 
-in vec3 vertPosition;
-in vec2 vertTexCoord;
-in vec3 vertNormal;
+in vec2 vertPosition;
 
-out vec4 fragTexCoord;
-out vec4 fragNormal;
-out vec4 fragPosition;
-
-uniform mat4 mWorld;
-uniform mat4 mView;
-uniform mat4 mProj;
+out vec2 vertCoordinates;
 
 void main()
 {
-  fragTexCoord = vec4(vertTexCoord, 0.0, 0.0);
-  fragNormal = vec4((mWorld * vec4(vertNormal, 0.0)).xyz, 0.0);
-
-  gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);
-  fragPosition = vec4(gl_Position.xyz, 0.0);
+  vertCoordinates = vertPosition;
+  gl_Position = vec4(vertPosition, 0.0, 1.0);
 }
